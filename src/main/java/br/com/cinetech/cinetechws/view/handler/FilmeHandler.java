01233a1c76d6c0e26ee9b68de5216a25c9e366ce
Jacqueline Oliveira <jacqueline.alterdata@gmail.com>
2021-08-17 22:02:06 -0300

    Finalização projeto hand-on

---
+++ src/main/java/br/com/cinetech/cinetechws/view/handler/FilmeHandler.java
@@ -0,0 +1,30 @@
+package br.com.cinetech.cinetechws.view.handler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class FilmeHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+            HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<String> mensagens = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(e -> e.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ResponseEntity<>(mensagens, HttpStatus.NOT_ACCEPTABLE);
+    }
+    
+    
+}
